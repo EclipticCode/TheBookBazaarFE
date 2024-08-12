ff27eda45ea4b30dc92f573afd58969aa4ab2f97
daylightcoderj6 <jananibharathi6@gmail.com>
2024-08-12 11:48:52 +0530

    created Socialmedia component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Marquee from './assets/Components/Marquee'
 import Feature from './assets/Components/Feature'
+import Socialmedia from './assets/Components/Socialmedia'
 
 const App = () => {
   return (
     <div>
      <Marquee/>
      <Feature/>
+     <Socialmedia/>
     </div>
   )
 }
---
+++ src/assets/Components/Socialmedia.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Socialmedia = () => {
+  return (
+    <div className="h-30 w-auto bg-zinc-900">
+      <h2 className="text-xl font-light text-white text-center p-4">
+        Join the #BBArmy
+      </h2>
+      <div className="flex justify-center">
+        <img
+          alt="facebook"
+          src="https://cdn.shopify.com/s/files/1/1002/7150/files/Social-Media-Icons1_134x134_crop_center@2x.jpg?v=1596170287"
+          width={80}
+          height={40}
+          className="max-h-12 object-contain"
+        />
+
+        <img
+          alt="Twitter"
+          src="https://cdn.shopify.com/s/files/1/1002/7150/files/Social-Media-Icons2_134x134_crop_center@2x.jpg?v=1596170289"
+          width={80}
+          height={40}
+          className="max-h-12 object-contain"
+        />
+
+        <img
+          alt="Instagram"
+          src="https://cdn.shopify.com/s/files/1/1002/7150/files/Social-Media-Icons3_134x134_crop_center@2x.jpg?v=1596170291"
+          width={80}
+          height={40}
+          className="max-h-12 object-contain"
+        />
+
+        <img
+          alt="You Tube"
+          src="https://cdn.shopify.com/s/files/1/1002/7150/files/Social-Media-Icons4_134x134_crop_center@2x.jpg?v=1596170293"
+          width={80}
+          height={40}
+          className="max-h-12 object-contain"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Socialmedia;
