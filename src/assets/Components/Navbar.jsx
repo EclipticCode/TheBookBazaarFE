fe12a2f73d53237aef43c00868a95215dcd0a4f0
daylightcoderj6 <jananibharathi6@gmail.com>
2024-08-13 07:37:10 +0530

    alignment check in Navbar

--- src/assets/Components/Navbar.jsx
+++ src/assets/Components/Navbar.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import bblogo from '../../assets/BBLOGO.svg'
+import React from "react";
+import bblogo from "../../assets/BBLOGO.svg";
 
 const Navbar = () => {
   return (
-    <div className=''>
-        <div className=''><img src={bblogo} width={100}
+    <div className="flex items-center justify-between m-4">
+      {/* logo - left side */}
+      <div className="flex-shrink-0">
+        <img
+          src={bblogo}
+          width={100}
           height={100}
-          className="max-h-12 object-contain"/></div>
-        <div className='flex justify-end'>
-        <div className='bg-yellow-450 text-white font-bold py-2 px-4 rounded mr-6'><button>Login</button></div>
-        <div className=''>Cart</div>
+          className="max-h-12 object-contain"
+        />
+      </div>
+      {/* Login and Cart - right side */}
+      <div className="flex items-center space-x-10 ml-auto">
+        <div className="bg-yellow-450 text-white font-bold py-1 px-4 rounded">
+          <button className="">Login</button>
         </div>
+        <div className="">Cart</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
